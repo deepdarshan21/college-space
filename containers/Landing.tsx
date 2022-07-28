05c43f94c38fde4e63a323e1c76d542021f7214d
Deepdarshan <deepdarshan21@gmail.com>
2022-07-28 15:20:50 +0530

    fix: add file to remove build issue
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

---
+++ containers/Landing.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Landing: React.FC<{}> = () => {
+    return (
+        <div className="w-full">
+            <div className="underline">deep</div>
+        </div>
+    );
+};
