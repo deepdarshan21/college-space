0af9286ff41e10d92ee8507497e768b580b50df4
Deepdarshan <deepdarshan21@gmail.com>
2022-07-30 12:24:00 +0530

    fix: add condition for not display nav and footer
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import PropTypes from "prop-types";
 
 import { Footer } from "@/layouts/Footer";
 import { Navbar } from "@/layouts/Navbar";
@@ -6,16 +7,28 @@ import { Navbar } from "@/layouts/Navbar";
 type LayoutProps = {
     meta: ReactNode;
     children: ReactNode;
+    displayNav?: boolean;
+    displayFooter?: boolean;
 };
 
 export const Layout = (props: LayoutProps) => (
     <div className="w-full min-h-screen pb-8 antialiased text-gray-700 bg-[#F3F2EF]">
         {props.meta}
 
-        <Navbar />
+        {props.displayNav && <Navbar />}
         <div className="mx-auto">
             <div className="">{props.children}</div>
         </div>
-        <Footer />
+        {props.displayFooter && <Footer />}
     </div>
 );
+
+Layout.propTypes = {
+    displayNav: PropTypes.string.isRequired,
+    displayFooter: PropTypes.string.isRequired,
+};
+
+Layout.defaultProps = {
+    displayNav: true,
+    displayFooter: true,
+};
