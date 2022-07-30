1dd628648238c1102c8437f473869e96c9b4b822
Deepdarshan <deepdarshan21@gmail.com>
2022-07-30 06:10:48 +0530

    feat: add avatar
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

---
+++ components/Avatar.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+type AvatarProp = {
+    size: number;
+    src?: string;
+    userName?: string;
+};
+
+export const Avatar = (props: AvatarProp) => (
+    <span className={`flex w-[${props.size}px] h-[${props.size}px] rounded-full`}>
+        <Image
+            src={require("@/images/sample-user.jpg")}
+            alt={props.userName ? props.userName : "User"}
+            // layout="fixed"
+            objectFit="contain"
+        />
+    </span>
+);
