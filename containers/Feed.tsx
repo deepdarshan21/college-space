f9af05ecbd737b40eda6c510182986a8b5e6b9de
Deepdarshan <deepdarshan21@gmail.com>
2022-07-31 11:38:11 +0530

    feat: add Article component
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- containers/Feed.tsx
+++ containers/Feed.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
-import {WritePost} from "@/components/WritePost";
+import { WritePost } from "@/components/WritePost";
+import { Article } from "@/components/Article";
 
 export const FeedContainer: React.FC<{}> = () => {
     return (
         <div className="flex flex-col items-center mx-auto w-[100vw] md:w-[75vw] lg:w-[60vw]">
             <WritePost />
+            <br />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+            <Article />
         </div>
     );
 };
