f4b78bf2ecd0ad4da865efd987c0654685920717
Deepdarshan <deepdarshan21@gmail.com>
2022-08-11 23:03:34 +0530

    fix: manage z-index
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -15,7 +15,7 @@ export const Popup = (props: PopupProps) => {
 
     return (
         <div
-            className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center"
+            className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center z-10"
             id="outer-container"
             onClick={handleClose}
         >
