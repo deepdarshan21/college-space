ec6e20ac37d7ea2f2f2d56e435c84ce6369e4358
Deepdarshan <deepdarshan21@gmail.com>
2022-12-05 12:30:33 +0530

    feat: update user preferance
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- server/graphql/resolvers/likePost.js
+++ server/graphql/resolvers/likePost.js
@@ -1,7 +1,7 @@
 const Post = require("./../../models/post");
+const User = require("./../../models/users");
 
 module.exports = async (args) => {
-    console.log(args);
     const {
         likeInput: { postId, username },
     } = args;
@@ -10,6 +10,15 @@ module.exports = async (args) => {
     if (post) {
         if (!post.likes.includes(username)) {
             await post.updateOne({ $push: { likes: username } });
+            const user = await User.findOne({ username });
+            const userTopics = user.topics;
+            const postTopics = post.topics;
+            userTopics.map((userTopic) => {
+                if (postTopics.includes(userTopic.topicName)) {
+                    userTopic.topicScore += 1;
+                }
+            });
+            await user.save();
             return "Post Liked";
         } else {
             await post.updateOne({ $pull: { likes: username } });
