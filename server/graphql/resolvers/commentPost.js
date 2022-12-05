0542c9a0999238c6522ce3ae6c75c76937af1d3a
Deepdarshan <deepdarshan21@gmail.com>
2022-12-05 12:44:59 +0530

    feat: increase score of topic on comment
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- server/graphql/resolvers/commentPost.js
+++ server/graphql/resolvers/commentPost.js
@@ -1,4 +1,5 @@
 const Post = require("./../../models/post");
+const User = require("./../../models/users");
 
 module.exports = async (args) => {
     const {
@@ -7,7 +8,16 @@ module.exports = async (args) => {
 
     const post = await Post.findById(postId);
     if (post) {
-        await post.updateOne({ $push: { comments: {body, username, createdAt: new Date()} } });
+        await post.updateOne({ $push: { comments: { body, username, createdAt: new Date() } } });
+        const user = await User.findOne({ username });
+        const userTopics = user.topics;
+        const postTopics = post.topics;
+        userTopics.map((userTopic) => {
+            if (postTopics.includes(userTopic.topicName)) {
+                userTopic.topicScore += 3;
+            }
+        });
+        await user.save();
         return "Comment on post";
     } else {
         return new Error("No post found");
