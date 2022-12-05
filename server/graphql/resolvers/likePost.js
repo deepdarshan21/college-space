b13b3b2f8ea88d6b0031be97fdcb55fb2bd0a372
Deepdarshan <deepdarshan21@gmail.com>
2022-12-05 12:40:20 +0530

    feat: decrease score when post disliked
    Signed-off-by: Deepdarshan <deepdarshan21@gmail.com>

--- server/graphql/resolvers/likePost.js
+++ server/graphql/resolvers/likePost.js
@@ -22,6 +22,15 @@ module.exports = async (args) => {
             return "Post Liked";
         } else {
             await post.updateOne({ $pull: { likes: username } });
+            const user = await User.findOne({ username });
+            const userTopics = user.topics;
+            const postTopics = post.topics;
+            userTopics.map((userTopic) => {
+                if (postTopics.includes(userTopic.topicName)) {
+                    userTopic.topicScore -= 1;
+                }
+            });
+            await user.save();
             return "Post Disliked";
         }
         console.log(post.likes);
